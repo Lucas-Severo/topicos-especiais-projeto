bc5ff6f432936ab29c30fc8ff9c8f63cd286157f
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2021-11-12 17:30:15 -0400

    [FEATURE] Buscar Retratos Usuário (#8)

--- portraitart-app/src/views/PerfilUsuario.vue
+++ portraitart-app/src/views/PerfilUsuario.vue
@@ -1,5 +1,9 @@
 <template>
     <v-container fluid>
+        <Modal 
+            v-model="dialog"
+            :image="retratoModal"
+            @closeDialog="fecharModal"/>
         <v-row justify="center" align="center">
             <v-col cols="12" sm="6">
                 <p v-if="!userExists">Usuário Não Encontrado</p>
@@ -11,37 +15,78 @@
                     <div class="d-flex">
                         <div>
                             <p class="mr-6"><span>{{user.name}}</span></p>
-                            <div>Retratos: {{user.retratos.length}}</div>
+                            <div>Retratos: {{quantidadeTotal}}</div>
                         </div>
                         <p class="font-weight-bold h6">{{user.username}}</p>
                     </div>
                 </div>
             </v-col>
         </v-row>
+        <div class="container mt-10" v-if="userExists">
+            <h2 class="mb-3">{{quantidadeTotal}} Retratos</h2>
+
+            <div class="text-center">
+                <v-pagination
+                    v-if="totalPages>0"
+                    v-model="page"
+                    class="mb-8"
+                    :length="totalPages"
+                    @input="buscarRetratosUsuario"
+                ></v-pagination>
+            </div>
+            <v-row class="row">
+                <v-img
+                    v-for="retrato in retratos"
+                    @click="() => abrirModalInfoImagem(retrato)"
+                    class="col-1 mr-4 mb-2 retrato"
+                    :key="retrato.id"
+                    :lazy-src="'http://localhost:1337'+retrato.imagem_baixa_definicao[0].url"
+                    min-height="150"
+                    min-width="250"
+                    max-height="150"
+                    max-width="250"
+                    :src="'http://localhost:1337'+retrato.imagem_baixa_definicao[0].url"/>
+                <div></div>
+            </v-row>
+        </div>
     </v-container>
 </template>
 
 <script>
 import UsuarioApiRequest from '../utils/UsuarioApiRequest'
+import RetratoApiRequest from '../utils/RetratoApiRequest'
 import ImageApiRequest from '../utils/ImageApiRequest'
 import ImagePicker from '../components/ImagePicker'
+import Modal from '../components/Modal'
 import store from '../store'
 
 export default {
     name: 'PerfilUsuario',
     components: {
-        ImagePicker
+        ImagePicker,
+        Modal
     },
-    data: () => ({
-        username: '',
-        userExists: false,
-        user: {}
-    }),
+    data() {
+        return {
+            username: '',
+            userExists: false,
+            user: {},
+            limit: 12,
+            page: 1,
+            retratos: [],
+            totalPages: 1,
+            quantidadeTotal: 0,
+            dialog: false,
+            retratoModal: {}
+        }
+    },
     watch: {
         '$route.params': {
             async handler() {
                 this.obterUserName()
                 await this.verificarDadosUsuario()
+                await this.buscarQuantidadeRetratos()
+                await this.buscarRetratosUsuario()
             },
             deep: true
         }
@@ -57,6 +102,8 @@ export default {
     async mounted() {
         this.obterUserName()
         await this.verificarDadosUsuario()
+        await this.buscarQuantidadeRetratos()
+        await this.buscarRetratosUsuario()
     },
     methods: {
         obterUserName() {
@@ -80,11 +127,48 @@ export default {
                 const response = await UsuarioApiRequest.atualizarUsuarioPorId(usuarioAlteracao.id, usuarioAlteracao)
                 this.user = {...response.data}
             }
+        },
+        async buscarQuantidadeRetratos() {
+            const {data: quantidadeTotal} = await RetratoApiRequest.countRetratosPorUsuario(
+                this.user.username
+            )
+            this.quantidadeTotal = quantidadeTotal
+        },
+        async buscarRetratosUsuario() {
+            const {data} = await RetratoApiRequest.buscarRetratosUsuarioPaginado(
+                this.user.username,
+                this.page,
+                this.limit
+            )
+            this.retratos = data
+            this.totalPages = Math.ceil(this.quantidadeTotal / this.limit)
+        },
+        abrirModalInfoImagem(imagem) {
+            this.retratoModal = imagem.imagem_baixa_definicao[0]
+            this.dialog = true
+        },
+        fecharModal() {
+            this.dialog = false
         }
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+    .retrato
+        position relative
+        cursor pointer
 
+    .retrato:hover::before 
+        content: "👁";
+        position: absolute
+        top 0
+        left 0
+        display: flex
+        justify-content: center
+        align-items: center
+        background-color #000a
+        color white
+        width 100%
+        height 100%
 </style>
