4ae002c40ea5026ec76fcb0526843855cad3618b
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2021-11-12 14:19:40 -0400

    Mostrar retratos por pessoa

--- portraitart-app/src/utils/RetratoApiRequest.js
+++ portraitart-app/src/utils/RetratoApiRequest.js
@@ -6,6 +6,15 @@ class RetratoApiRequest {
         return axios.get('/retratos')
     }
 
+    async buscarRetratosUsuarioPaginado(username, page, limit) {
+        const start = (page - 1) * limit
+        return axios.get(`/retratos?usuario.username=${username}&_start=${start}&_limit=${limit}&_sort=created_at:desc`)
+    }
+
+    async countRetratosPorUsuario(username) {
+        return axios.get(`/retratos/count?usuario.username=${username}`)
+    }
+
 }
 
 export default new RetratoApiRequest()
--- portraitart-app/src/views/PerfilUsuario.vue
+++ portraitart-app/src/views/PerfilUsuario.vue
@@ -11,18 +11,45 @@
                     <div class="d-flex">
                         <div>
                             <p class="mr-6"><span>{{user.name}}</span></p>
-                            <div>Retratos: {{user.retratos.length}}</div>
+                            <div>Retratos: {{quantidadeTotal}}</div>
                         </div>
                         <p class="font-weight-bold h6">{{user.username}}</p>
                     </div>
                 </div>
             </v-col>
         </v-row>
+        <div class="container mt-10" v-if="userExists">
+            <h2 class="mb-3">{{quantidadeTotal}} Retratos</h2>
+
+            <div class="text-center">
+                <v-pagination
+                    v-if="totalPages>0"
+                    v-model="page"
+                    class="mb-8"
+                    :length="totalPages"
+                    @input="buscarRetratosUsuario"
+                ></v-pagination>
+            </div>
+            <v-row class="row">
+                <v-img
+                    v-for="retrato in retratos" 
+                    class="col-1 mr-4 mb-2 retrato"
+                    :key="retrato.id"
+                    :lazy-src="'http://localhost:1337'+retrato.imagem[0].url"
+                    min-height="150"
+                    min-width="250"
+                    max-height="150"
+                    max-width="250"
+                    :src="'http://localhost:1337'+retrato.imagem[0].url"/>
+                <div></div>
+            </v-row>
+        </div>
     </v-container>
 </template>
 
 <script>
 import UsuarioApiRequest from '../utils/UsuarioApiRequest'
+import RetratoApiRequest from '../utils/RetratoApiRequest'
 import ImageApiRequest from '../utils/ImageApiRequest'
 import ImagePicker from '../components/ImagePicker'
 import store from '../store'
@@ -35,13 +62,20 @@ export default {
     data: () => ({
         username: '',
         userExists: false,
-        user: {}
+        user: {},
+        limit: 12,
+        page: 1,
+        retratos: [],
+        totalPages: 1,
+        quantidadeTotal: 0
     }),
     watch: {
         '$route.params': {
             async handler() {
                 this.obterUserName()
                 await this.verificarDadosUsuario()
+                await this.buscarQuantidadeRetratos()
+                await this.buscarRetratosUsuario()
             },
             deep: true
         }
@@ -57,6 +91,8 @@ export default {
     async mounted() {
         this.obterUserName()
         await this.verificarDadosUsuario()
+        await this.buscarQuantidadeRetratos()
+        await this.buscarRetratosUsuario()
     },
     methods: {
         obterUserName() {
@@ -80,11 +116,41 @@ export default {
                 const response = await UsuarioApiRequest.atualizarUsuarioPorId(usuarioAlteracao.id, usuarioAlteracao)
                 this.user = {...response.data}
             }
+        },
+        async buscarQuantidadeRetratos() {
+            const {data: quantidadeTotal} = await RetratoApiRequest.countRetratosPorUsuario(
+                this.user.username
+            )
+            this.quantidadeTotal = quantidadeTotal
+        },
+        async buscarRetratosUsuario() {
+            const {data} = await RetratoApiRequest.buscarRetratosUsuarioPaginado(
+                this.user.username,
+                this.page,
+                this.limit
+            )
+            this.retratos = data
+            this.totalPages = Math.ceil(this.quantidadeTotal / this.limit)
         }
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+    .retrato
+        position relative
+        cursor pointer
 
+    .retrato:hover::before 
+        content: "👁";
+        position: absolute
+        top 0
+        left 0
+        display: flex
+        justify-content: center
+        align-items: center
+        background-color #000a
+        color white
+        width 100%
+        height 100%
 </style>
