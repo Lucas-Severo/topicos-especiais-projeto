d8277f280fd4749bab30ee3e7ae7dd280b14eb68
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2021-11-26 22:45:04 -0400

    Adicionado Content Provider

--- portraitart-app/src/components/FullScreenImage.vue
+++ portraitart-app/src/components/FullScreenImage.vue
@@ -51,7 +51,7 @@ export default {
                 return URL.createObjectURL(imagemFullScreen)
             }
             if (imagemFullScreen) {
-                return ConfigProvider.getBaseUrl() + imagemFullScreen.url
+                return ConfigProvider.getFileProviderUrl() + imagemFullScreen.url
             }
         },
         mostrarImagemFullScreen() {
